b4ed3b0ecf96d09db762a3ab594446a08a2d5742
Dima Karpenko dimak <dimka2010@gmail.com>
2025-12-10 09:55:00 +0300

    refactor: remove old src/ flat structure
    Old layout (src/Core, src/Application, src/Infrastructure, test/) replaced
    by Clean Architecture without src/ wrapper:
      LogCollector.Core/
      LogCollector.Application/
      LogCollector.Infrastructure/
      LogCollector.Host/
      LogCollector.Tests/
    
    LogEntry, ILogParser, ILogRepository, BatchWriteService, UdpLogListener
    rewritten from scratch with zero-allocation Kokosa/Richter/Cleary patterns.

--- src/LogCollector.Core/Interfaces/ILogParser.cs
+++
@@ -1,9 +0,0 @@
-﻿using LogCollector.Core.Models;
-using System.Net;
-
-namespace LogCollector.Core.Interfaces;
-
-public interface ILogParser
-{
-    bool TryParse(ReadOnlyMemory<byte> data, IPEndPoint remoteEndpoint, out LogEntry logEntry);
-}
